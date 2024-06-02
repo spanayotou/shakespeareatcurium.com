6575e040ebedadd80a7633758bb7c4e5ac0d725f
Sophia Panayotou <sophia@bridgerpay.com>
2024-06-02 20:37:09 +0300

    header fix

--- components/blocks/hero.tsx
+++ components/blocks/hero.tsx
@@ -1,196 +1,152 @@
-import * as React from "react";
-import { Actions } from "../util/actions";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { Container } from "../util/container";
-import { Section } from "../util/section";
-import { useTheme } from "../layout";
-import { TinaMarkdown } from "tinacms/dist/rich-text";
-import type { TinaTemplate } from "tinacms";
-import { PageBlocksHero } from "../../tina/__generated__/types";
+import { useTheme } from "."; // Assuming this imports the theme hook
 import { tinaField } from "tinacms/dist/react";
+import { GlobalHeader } from "../../tina/__generated__/types";
 
-export const Hero = ({ data }: { data: PageBlocksHero }) => {
-  const theme = useTheme();
-  const headlineColorClasses = {
-    blue: "from-blue-400 to-blue-600",
-    teal: "from-teal-400 to-teal-600",
-    green: "from-green-400 to-green-600",
-    red: "from-red-400 to-red-600",
-    pink: "from-pink-400 to-pink-600",
-    purple: "from-purple-400 to-purple-600",
-    orange: "from-orange-300 to-orange-600",
-    yellow: "from-yellow-400 to-yellow-600",
+export const Header = ({ data }: { data: GlobalHeader }) => {
+  const router = useRouter();
+  const theme = useTheme(); // Assuming this is a custom hook to get the theme
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  const headerColor = {
+    default: "text-black dark:text-white from-gray-50 to-white dark:from-gray-800 dark:to-gray-900",
+    primary: {
+      blue: "text-white from-blue-300 to-blue-500",
+      teal: "text-white from-teal-400 to-teal-500",
+      green: "text-white from-green-400 to-green-500",
+      red: "text-white from-red-400 to-red-500",
+      pink: "text-white from-pink-400 to-pink-500",
+      purple: "text-white from-purple-400 to-purple-500",
+      orange: "text-white from-orange-400 to-orange-500",
+      yellow: "text-white from-yellow-400 to-yellow-500",
+    },
+  };
+
+  const headerColorCss = data.color === "primary" ? headerColor.primary[theme.color] : headerColor.default;
+
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    setIsMobile(mediaQuery.matches);
+    const handler = (e: MediaQueryListEvent) => setIsMobile(e.matches);
+    mediaQuery.addListener(handler);
+    return () => mediaQuery.removeListener(handler);
+  }, []);
+
+  const handleMenuItemClick = () => {
+    setMenuOpen(false); // Close the menu when a menu item is clicked
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
-    <Section color={data.color}>
-      <Container
-        size="large"
-        className="grid grid-cols-1 md:grid-cols-5 gap-14 items-center justify-center"
-      >
-        <div className="row-start-2 md:row-start-1 md:col-span-3 text-center md:text-left">
-          {data.tagline && (
-            <h2
-              data-tina-field={tinaField(data, "tagline")}
-              className="relative inline-block px-3 py-1 mb-8 text-md font-bold tracking-wide title-font z-20"
-            >
-              {data.tagline}
-              <span className="absolute w-full h-full left-0 top-0 rounded-full -z-1 bg-current opacity-7"></span>
-            </h2>
-          )}
-          {data.headline && (
-            <h3
-              data-tina-field={tinaField(data, "headline")}
-              className={`w-full relative	mb-10 text-5xl font-extrabold tracking-normal leading-tight title-font`}
-            >
-              <span
-                className={`bg-clip-text text-transparent bg-gradient-to-r  ${
-                  data.color === "primary"
-                    ? `from-white to-gray-100`
-                    : headlineColorClasses[theme.color]
-                }`}
+    <div className={`relative overflow-hidden bg-gradient-to-b ${headerColorCss}`}>
+      <Container size="custom" className="py-0 relative z-10 max-w-8xl">
+        <div className="flex items-center justify-between gap-6">
+          <h4 className="select-none text-lg font-bold tracking-tight my-4 transition duration-150 ease-out transform">
+            {isMobile ? (
+              <Link href="/" className="flex gap-1 items-center whitespace-nowrap tracking-[.002em]">
+                <img
+                  src={data.image as string}
+                  alt={data.name}
+                  className="w-26 h-20"
+                />
+                <span data-tina-field={tinaField(data, "name")}>{data.name}</span>
+              </Link>
+            ) : (
+              <Link href="/" className="flex gap-1 items-center whitespace-nowrap tracking-[.002em]">
+                <img
+                  src={data.image as string}
+                  alt={data.name}
+                  className="w-26 h-20"
+                />
+                <span data-tina-field={tinaField(data, "name")}>{data.name}</span>
+              </Link>
+            )}
+          </h4>
+          {isMobile && (
+            <div className="lg:hidden">
+              <button
+                onClick={toggleMenu}
+                className="text-xl focus:outline-none"
               >
-                {data.headline}
-              </span>
-            </h3>
-          )}
-
-
-          {data.text && (
-            <div
-              data-tina-field={tinaField(data, "text")}
-              className={`prose prose-lg mx-auto md:mx-0 mb-10 ${
-                data.color === "primary" ? `prose-primary` : `dark:prose-dark`
-              }`}
-            >
-              <TinaMarkdown content={data.text} />
+                {menuOpen ? "✕" : "☰"}
+              </button>
             </div>
           )}
-
-          
-          {data.actions && (
-            <Actions
-              className="justify-center md:justify-start py-2"
-              parentColor={data.color}
-              actions={data.actions}
-            />
-          )}
         </div>
-        {data.image && (
-          <div
-            data-tina-field={tinaField(data.image, "src")}
-            className="relative row-start-1 md:col-span-2 flex justify-center"
-          >
-            <img
-              className="absolute w-full rounded-lg max-w-xs md:max-w-none h-auto blur-2xl brightness-150 contrast-[0.9] dark:brightness-150 saturate-200 opacity-50 dark:opacity-30 mix-blend-multiply dark:mix-blend-hard-light"
-              src={data.image.src}
-              aria-hidden="true"
-            />
-            <img
-              className="relative z-10 w-full max-w-xs rounded-lg md:max-w-none h-auto"
-              alt={data.image.alt}
-              src={data.image.src}
-            />
+        {menuOpen && (
+          <div className="fixed top-0 left-0 w-full h-full bg-white bg-opacity-95 z-50 flex justify-center items-center">
+            <ul className="flex flex-col gap-4 p-4">
+              {data.nav &&
+                data.nav.map((item, i) => {
+                  const activeItem = (item.href === "" ? router.asPath === "/" : router.asPath.includes(item.href)) && isClient;
+                  return (
+                    <li key={`${item.label}-${i}`} className={`${activeItem ? "active" : ""}`}>
+                      <Link
+                        data-tina-field={tinaField(item, "label")}
+                        href={`/${item.href}`}
+                        className={`relative select-none text-base inline-block tracking-wide transition duration-150 ease-out hover:opacity-100 py-2 px-4 ${activeItem ? `` : `opacity-70`}`}
+                        onClick={handleMenuItemClick} // Close menu when menu item is clicked
+                      >
+                        {item.label}
+                        {activeItem && (
+                          <svg
+                            className={`absolute bottom-0 left-1/2 w-[180%] h-full -translate-x-1/2 -z-1 opacity-10 dark:opacity-15`}
+                            preserveAspectRatio="none"
+                            viewBox="0 0 230 230"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <rect
+                              x="230"
+                              y="230"
+                              width="230"
+                              height="230"
+                              transform="rotate(-180 230 230)"
+                              fill="url(#paint0_radial_1_33)"
+                            />
+                            <defs>
+                              <radialGradient
+                                id="paint0_radial_1_33"
+                                cx="0"
+                                cy="0"
+                                r="1"
+                                gradientUnits="userSpaceOnUse"
+                                gradientTransform="translate(345 230) rotate(90) scale(230 115)"
+                              >
+                                <stop stopColor="currentColor" />
+                                <stop
+                                  offset="1"
+                                  stopColor="currentColor"
+                                  stopOpacity="0"
+                                />
+                              </radialGradient>
+                            </defs>
+                          </svg>
+                        )}
+                      </Link>
+                    </li>
+                  );
+                })}
+            </ul>
           </div>
         )}
+        <div
+          className={`absolute h-1 bg-gradient-to-r from-transparent ${
+            data.color === "primary" ? `via-white` : `via-black dark:via-white`
+          } to-transparent bottom-0 left-4 right-4 -z-1 opacity-5`}
+        />
       </Container>
-    </Section>
+    </div>
   );
 };
 
-export const heroBlockSchema: TinaTemplate = {
-  name: "hero",
-  label: "Hero",
-  ui: {
-    previewSrc: "/blocks/hero.png",
-    defaultItem: {
-      tagline: "Here's some text above the other text",
-      headline: "This Big Text is Totally Awesome",
-      text: "Phasellus scelerisque, libero eu finibus rutrum, risus risus accumsan libero, nec molestie urna dui a leo.",
-    },
-  },
-  fields: [
-    {
-      type: "string",
-      label: "Tagline",
-      name: "tagline",
-    },
-    {
-      type: "string",
-      label: "Headline",
-      name: "headline",
-    },
-    {
-      label: "Text",
-      name: "text",
-      type: "rich-text",
-    },
-    {
-      label: "Actions",
-      name: "actions",
-      type: "object",
-      list: true,
-      ui: {
-        defaultItem: {
-          label: "Action Label",
-          type: "button",
-          icon: true,
-          link: "/",
-        },
-        itemProps: (item) => ({ label: item.label }),
-      },
-      fields: [
-        {
-          label: "Label",
-          name: "label",
-          type: "string",
-        },
-        {
-          label: "Type",
-          name: "type",
-          type: "string",
-          options: [
-            { label: "Button", value: "button" },
-            { label: "Link", value: "link" },
-          ],
-        },
-        {
-          label: "Icon",
-          name: "icon",
-          type: "boolean",
-        },
-        {
-          label: "Link",
-          name: "link",
-          type: "string",
-        },
-      ],
-    },
-    {
-      type: "object",
-      label: "Image",
-      name: "image",
-      fields: [
-        {
-          name: "src",
-          label: "Image Source",
-          type: "image",
-        },
-        {
-          name: "alt",
-          label: "Alt Text",
-          type: "string",
-        },
-      ],
-    },
-    {
-      type: "string",
-      label: "Color",
-      name: "color",
-      options: [
-        { label: "Default", value: "default" },
-        { label: "Tint", value: "tint" },
-        { label: "Primary", value: "primary" },
-      ],
-    },
-  ],
-};
+export default Header;
