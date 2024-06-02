63d46bc52195a34a4aadf7d8c5de3df038e13459
Sophia Panayotou <sophia@bridgerpay.com>
2024-06-02 21:32:01 +0300

    desktop fix try again

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -48,19 +48,17 @@ export const Header = ({ data }: { data: GlobalHeader }) => {
     <div className={`relative overflow-hidden bg-gradient-to-b ${headerColorCss}`}>
       <Container size="custom" className="py-0 relative z-10 max-w-8xl">
         <div className="flex items-center justify-between gap-6">
-          {!isMobile && (
-            <h4 className="select-none text-lg font-bold tracking-tight my-4 transition duration-150 ease-out transform">
-              <Link href="/" className="flex gap-1 items-center whitespace-nowrap tracking-[.002em]">
-                <img
-                  src={data.image as string}
-                  alt={data.name}
-                  className="w-26 h-20"
-                />
-                <span data-tina-field={tinaField(data, "name")}>{data.name}</span>
-              </Link>
-            </h4>
-          )}
-          <nav className="hidden lg:block">
+          <h4 className={`select-none text-lg font-bold tracking-tight my-4 transition duration-150 ease-out transform ${isMobile ? 'hidden lg:block' : ''}`}>
+            <Link href="/" className="flex gap-1 items-center whitespace-nowrap tracking-[.002em]">
+              <img
+                src={data.image as string}
+                alt={data.name}
+                className="w-26 h-20"
+              />
+              <span data-tina-field={tinaField(data, "name")}>{data.name}</span>
+            </Link>
+          </h4>
+          <nav className={`lg:flex ${isMobile ? 'hidden' : 'block'}`}>
             <ul className="flex gap-4">
               {data.nav &&
                 data.nav.map((item, i) => (
@@ -98,63 +96,62 @@ export const Header = ({ data }: { data: GlobalHeader }) => {
                     <li key={`${item.label}-${i}`} className={`${activeItem ? "active" : ""}`}>
                       <Link
                         data-tina-field={tinaField(item, "label")}
-                        href={`/${item.href}`}
-                        className={`relative select-none text-base inline-block tracking-wide transition duration-150 ease-out hover:opacity-100 py-2 px-4 ${activeItem ? `` : `opacity-70`}`}
-                        onClick={handleMenuItemClick} // Close menu when menu item is clicked
-                      >
-                        {item.label}
-                        {activeItem && (
-                          <svg
-                            className={`absolute bottom-0 left-1/2 w-[180%] h-full -translate-x-1/2 -z-1 opacity-10 dark:opacity-15`}
-                            preserveAspectRatio="none"
-                            viewBox="0 0 230 230"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <rect
-                              x="230"
-                              y="230"
-                              width="230"
-                              height="230"
-                              transform="rotate(-180 230 230)"
-                              fill="url(#paint0_radial_1_33)"
-                            />
-                            <defs>
-                              <radialGradient
-                                id="paint0_radial_1_33"
-                                cx="0"
-                                cy="0"
-                                r="1"
-                                gradientUnits="userSpaceOnUse"
-                                gradientTransform="translate(345 230) rotate(
-                                  scale(230 115)"
-                                  >
-                                    <stop stopColor="currentColor" />
-                                    <stop
-                                      offset="1"
-                                      stopColor="currentColor"
-                                      stopOpacity="0"
-                                    />
-                                  </radialGradient>
-                                </defs>
-                              </svg>
-                            )}
-                          </Link>
-                        </li>
-                      );
-                    })}
-                </ul>
-              </div>
-            )}
-            <div
-              className={`absolute h-1 bg-gradient-to-r from-transparent ${
-                data.color === "primary" ? `via-white` : `via-black dark:via-white`
-              } to-transparent bottom-0 left-4 right-4 -z-1 opacity-5`}
-            />
-          </Container>
-        </div>
-      );
-    };
-      
-    export default Header;
-    
+                        href={`/${
+                          item.href}`}
+                          className={`relative select-none text-base inline-block tracking-wide transition duration-150 ease-out hover:opacity-100 py-2 px-4 ${activeItem ? `` : `opacity-70`}`}
+                          onClick={handleMenuItemClick} // Close menu when menu item is clicked
+                        >
+                          {item.label}
+                          {activeItem && (
+                            <svg
+                              className={`absolute bottom-0 left-1/2 w-[180%] h-full -translate-x-1/2 -z-1 opacity-10 dark:opacity-15`}
+                              preserveAspectRatio="none"
+                              viewBox="0 0 230 230"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <rect
+                                x="230"
+                                y="230"
+                                width="230"
+                                height="230"
+                                transform="rotate(-180 230 230)"
+                                fill="url(#paint0_radial_1_33)"
+                              />
+                              <defs>
+                                <radialGradient
+                                  id="paint0_radial_1_33"
+                                  cx="0"
+                                  cy="0"
+                                  r="1"
+                                  gradientUnits="userSpaceOnUse"
+                                  gradientTransform="translate(345 230) rotate(90) scale(230 115)"
+                                >
+                                  <stop stopColor="currentColor" />
+                                  <stop
+                                    offset="1"
+                                    stopColor="currentColor"
+                                    stopOpacity="0"
+                                  />
+                                </radialGradient>
+                              </defs>
+                            </svg>
+                          )}
+                        </Link>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+          )}
+          <div
+            className={`absolute h-1 bg-gradient-to-r from-transparent ${
+              data.color === "primary" ? `via-white` : `via-black dark:via-white`
+            } to-transparent bottom-0 left-4 right-4 -z-1 opacity-5`}
+          />
+        </Container>
+      </div>
+    );
+  };
+  
+  export default Header;
