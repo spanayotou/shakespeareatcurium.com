7831bb02dd949cdf0c47ae3cbd507f15918fb5d3
Sophia Panayotou <sophia@bridgerpay.com>
2024-11-02 11:42:28 +0200

    tab name

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -18,7 +18,7 @@ export const Layout = ({
   return (
     <>
       <Head>
-        <title>Tina</title>
+        <title>Shakespeare at Kourion</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         {data.theme.font === "nunito" && (
           <>
