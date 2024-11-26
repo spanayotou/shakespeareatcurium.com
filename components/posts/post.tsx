e432eaaef666fffd0b9f224c018f58ed15b467c8
Sophia Panayotou <sophia@bridgerpay.com>
2024-11-26 08:03:56 +0200

    fix

--- components/posts/post.tsx
+++ components/posts/post.tsx
@@ -1,16 +1,3 @@
-/**
-Copyright 2021 Forestry.io Holdings, Inc.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 import React from "react";
 import { Container } from "../util/container";
 import { Section } from "../util/section";
@@ -34,6 +21,7 @@ const components: Components<{
   };
 }> = {
   code_block: (props) => <Prism {...props} />,
+  
   BlockQuote: (props: {
     children: TinaMarkdownContent;
     authorName: string;
@@ -47,6 +35,7 @@ const components: Components<{
       </div>
     );
   },
+  
   NewsletterSignup: (props) => {
     return (
       <div className="bg-white">
@@ -85,10 +74,21 @@ const components: Components<{
       </div>
     );
   },
+  
   img: (props) => (
     <span className="flex items-center justify-center">
       <img src={props.url} alt={props.alt} />
     </span>
+  ),
+
+  // Custom hyperlink component for proper styling
+  a: (props) => (
+    <a
+      href={props.href}
+      className="text-blue-500 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    >
+      {props.children}
+    </a>
   ),
 };
 
@@ -116,9 +116,7 @@ export const Post = (props: PostType) => {
           className={`w-full relative mb-8 text-4xl font-extrabold tracking-normal text-center title-font`}
         >
           <span
-            className={`bg-clip-text text-transparent bg-gradient-to-r ${
-              titleColorClasses[theme.color]
-            }`}
+            className={`bg-clip-text text-transparent bg-gradient-to-r ${titleColorClasses[theme.color]}`}
           >
             {props.title}
           </span>
